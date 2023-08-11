7debd26a4306ed5ea45141e01da9d0ca42669609
aristata <aristatait@gmail.com>
2023-08-11 14:35:14 +0900

    feat: 튜토리얼 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import './App.global.css'
-import styles from './App.module.css'
+import "./App.global.css";
+import styles from "./App.module.css";
 
-import { Navigation } from './components/Navigation'
-import { Display } from './components/Display'
-import { MetaMaskError } from './components/MetaMaskError'
+import { Navigation } from "./components/Navigation";
+import { Display } from "./components/Display";
+import { MetaMaskError } from "./components/MetaMaskError";
+import { MetaMaskContextProvider } from "./hooks/useMetaMask";
 
 export const App = () => {
-
   return (
-    <div className={styles.appContainer}>
-      <Navigation />
-      <Display />
-      <MetaMaskError />
-    </div>
-  )
-}
+    <MetaMaskContextProvider>
+      <div className={styles.appContainer}>
+        <Navigation />
+        <Display />
+        <MetaMaskError />
+      </div>
+    </MetaMaskContextProvider>
+  );
+};
